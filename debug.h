cdced037f9f68f61aac26e7b3fcdf3e4c79f4952
Antoine Viallon <antoine.viallon@gmail.com>
2020-06-09 00:53:42 +0200

    Improve performance of debug macros

--- debug.h
+++ debug.h
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <knightos/display.h>
 
-#define TIMEOUT 100
+#define TIMEOUT 10
 
 #define debug_hinted(screen, msg, hint, draw_func) \
 { \
@@ -12,16 +12,18 @@
     const unsigned char* bug_msg = hint; \
     const unsigned short bug_msg_len = strlen(bug_msg); \
     const unsigned short space = (bug_msg_len != 0) ? 4 : 0; \
+    screen_clear(screen); \
+    draw_string(screen, 2, 2, "AT L. "); \
+    draw_short(screen, 2+6*4, 2, __LINE__); \
+    draw_string(screen, 2, 8*7, __FILE__); \
+    draw_string(screen, 2, 8*1, hint);\
+    draw_func(screen, 2+bug_msg_len*4+space, 8*1, msg); \
     for(i=0; i<TIMEOUT;i++){ \
-        screen_clear(screen); \
-        draw_string(screen, 2, 2, "AT L. "); \
-        draw_short(screen, 2+6*4, 2, __LINE__); \
-        draw_string(screen, 2, 8*7, __FILE__); \
-        draw_string(screen, 2, 8*1, hint);\
-        draw_func(screen, 2+bug_msg_len*4+space, 8*1, msg); \
+        draw_rectangle(screen, 8, 60, 8*6, 0, GFX_AND); \
         draw_short(screen, 2, 8*6, TIMEOUT-i); \
         screen_draw(screen); \
     } \
+    screen_clear(screen); \
 }
 
 #define debug(screen, msg, draw_func) debug_hinted(screen, msg, "", draw_func)
@@ -30,10 +32,6 @@
 #define debug_short(screen, msg) debug(screen, msg, draw_short)
 #define debug_signed(screen, msg) debug(screen, msg, draw_signed)
 #define debug_float(screen, msg) debug(screen, msg, draw_float)
-#define debug_float_hinted(screen, msg, hint) \
-{ \
-    float ___temp___ = msg; \
-    debug_hinted(screen, ___temp___, hint, draw_float); \
-}
+#define debug_float_hinted(screen, msg, hint) debug_hinted(screen, msg, hint, draw_float)
 
 #endif
