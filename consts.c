bb1de762d8fe65a9fe8f9343cfcabc39444d2d38
Antoine Viallon <antoine.viallon@gmail.com>
2020-06-11 02:47:26 +0200

    Delete useless consts.c

--- consts.c
+++
@@ -1,8 +0,0 @@
-// #include "consts.h"
-// 
-// void log(unsigned char* message){
-//     static const unsigned short logsize = 10;
-//     static const unsigned short message_len = 128;
-//     static unsigned char** LOG = calloc(logsize, message_len);
-//     static unsigned short LOG_len
-// }
